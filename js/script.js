95d65ea2e494b856708be35d5fe3e2adc8a68b31
sirri1222 <hyeonji159753@gmail.com>
2023-04-12 11:38:22 +0900

    모달창 완료

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 window.onload = function () {
+  // 모달창 기능
+  let modalWrap = document.querySelector(".modal-wrap");
+  let modalClose = document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modalWrap.classList.add("fadeOut");
+    modalWrap.addEventListener("animationend", () => {
+      modalWrap.style.display = "none";
+    });
+  });
+ 
+  
   // 콤마기능
   function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
